66426f1c48613b64a5c649e60f7a8441181a454f
Raehan123 <nurrayhan62@gmail.com>
2025-06-23 10:12:11 +0700

    UPDATE UAS Project NFPreloved

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    build: {
-        outDir: 'public/build',
-        emptyOutDir: true,
-    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/imagePreview.js",
+                "resources/js/heartToggle.js",
+                "resources/js/popup.js",
+                "resources/js/popupDelete.js",
+                "resources/js/toggle.js",
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: "public/build",
+        manifest: true,
+        emptyOutDir: true,
+    },
 });
